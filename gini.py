b554b5dc0a9064ab61f5ac0c9e2e5d56a6c7e614
Kiki Liu <kikiliu@ischool.berkeley.edu>
2014-02-27 13:35:31 -0800

    Correct floating conversion and Add output file

--- gini.py
+++ gini.py
@@ -5,12 +5,15 @@ Classes are strings."""
 
 import fileinput
 import csv
-import collections
+from collections import defaultdict, Counter
 
 
 def calc_gini(a, b):
-    gini = 1.0 - (a**2 + b**2)/(a+b)**2*1.0
-    return gini
+    if (a + b) > 0:
+        gini = 1 - 1.0*(a**2 + b**2)/(a+b)**2 #float to start, not work at the end
+        return gini
+    else:
+        return -1
 
 #mapping attribute to names
 (
@@ -27,10 +30,9 @@ CANDIDATES = {
 
 ############### Set up variables
 # TODO: declare datastructures
-oba_zip_dic = {}
-rom_zip_dic = {}
-zip_list = []
-
+oba_zip_dic = defaultdict(int)
+rom_zip_dic = defaultdict(int)
+zip_set = Counter()
 oba_count = 0
 rom_count = 0
 ############### Read through files
@@ -41,50 +43,40 @@ for row in csv.reader(fileinput.input(), delimiter='|'):
 
     candidate_name = CANDIDATES[candidate_id]
     zip_code = row[ZIP_CODE]
-
- 	###
+    ###
     # TODO: save information to calculate Gini Index
     ##/
-    
+    zip_set.update([zip_code])
     if candidate_name == 'Obama':
-    	oba_count += 1
-    	if zip_code not in oba_zip_dic.keys():
-	    oba_zip_dic[zip_code] = 1
-	    zip_list.append(zip_code)
-	else:
-	    oba_zip_dic[zip_code] += 1
+        oba_count += 1
+        oba_zip_dic[zip_code] += 1
     else:
-	rom_count += 1
-	if zip_code not in rom_zip_dic.keys():
-	    rom_zip_dic[zip_code] = 1
-	    zip_list.append(zip_code)
-	else:
-	    rom_zip_dic[zip_code] += 1
-zip_set = set(zip_list)
+        rom_count += 1
+        rom_zip_dic[zip_code] += 1
 ###
 # TODO: calculate the values below:
 gini = 0  # current Gini Index using candidate name as the class
-split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
+split_gini = 1  # weighted average of the Gini Indexes using candidate names, split up by zip code
 ##/
 ##gini stands for the worst case 
 gini = calc_gini(oba_count,rom_count)
 
-for zip_code in zip_set:
+for zip_code in zip_set.keys():
     if zip_code in oba_zip_dic.keys():
         oba_zip_count = oba_zip_dic[zip_code]
     else:
-	oba_zip_count = 0
+        oba_zip_count = 0
     if zip_code in rom_zip_dic.keys():
-	rom_zip_count = rom_zip_dic.get(zip_code)
+        rom_zip_count = rom_zip_dic.get(zip_code)
     else:
-	rom_zip_count = 0
+        rom_zip_count = 0
     zip_count = oba_zip_count + rom_zip_count
-    total_count = oba_count + rom_count	
+    total_count = oba_count + rom_count    
     zip_gini = calc_gini(oba_zip_count, rom_zip_count)
     rest_gini = calc_gini(oba_count - oba_zip_count, rom_count - rom_zip_count)
-    weighted_gini = zip_gini * zip_count / total_count + rest_gini * (total_count - zip_gini) / total_count
-    if weighted_gini > split_gini:
-	split_gini = weighted_gini
+    weighted_gini = (zip_gini * zip_count + rest_gini * (total_count - zip_count)) / total_count
+    if weighted_gini < split_gini:
+        split_gini = weighted_gini
 
 print "Gini Index: %s" % gini
 print "Gini Index after split: %s" % split_gini
